1b1831d2f93079c1500818517169307661772ad3
Mohd Imran Hossain <hossainmohdimran@gmail.com>
2026-02-06 13:43:56 +0600

    update index.js for route loader

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
+
+/* middlewares */
 app.use(cors());
 app.use(express.json());
 
+/* DB */
 mongoose.connect(process.env.MONGO_URI)
-  .then(()=>console.log('MongoDB connected'))
-  .catch(err=>console.error(err));
+  .then(() => console.log("MongoDB connected"))
+  .catch(console.error);
 
-app.get('/', (req, res) => res.send({ status: 'ScholarStream API running' }));
+/* routes */
+app.use("/api/users", require("./routes/user.routes"));
+app.use("/api/scholarships", require("./routes/scholarship.routes"));
+app.use("/api/applications", require("./routes/application.routes"));
+app.use("/api/reviews", require("./routes/review.routes"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>console.log(`Server running on ${PORT}`));
+
+app.get("/", (_, res) => res.send("ScholarStream API running"));
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on ${process.env.PORT}`)
+);
+
